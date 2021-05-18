c92da899fb617e9cb6757624e3801a9fe0e81fae
maxnth <maximilian.noeth@protonmail.com>
2021-05-18 23:45:15 +0200

    add support for schema location URLs which redirect

--- java/PrimaIo/src/org/primaresearch/io/xml/XmlValidator.java
+++ java/PrimaIo/src/org/primaresearch/io/xml/XmlValidator.java
@@ -17,29 +17,33 @@ package org.primaresearch.io.xml;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.JarURLConnection;
 import java.net.URL;
+import java.net.URLConnection;
 
 import javax.xml.XMLConstants;
-import javax.xml.transform.Source;
+import javax.xml.transform.*;
 import javax.xml.transform.stream.StreamSource;
 import javax.xml.validation.Schema;
 import javax.xml.validation.SchemaFactory;
 
 import org.xml.sax.SAXException;
 
+
 /**
  * XML validator for a specific schema.<br>
- * Validators are usually managed by a ValidatorProvider. 
- * 
+ * Validators are usually managed by a ValidatorProvider.
+ *
  * @author Christian Clausner
  *
  */
 public class XmlValidator {
-	
+
 	URL schemaSource;
 	Schema schema = null;
 	XmlFormatVersion schemaVersion;
-	
+
 	public XmlValidator(URL schemaSource, XmlFormatVersion schemaVersion) {
 		this.schemaSource = schemaSource;
 		this.schemaVersion = schemaVersion;
@@ -50,18 +54,43 @@ public class XmlValidator {
 	 * @return Schema location
 	 */
 	public URL getSchemaSource() {
-		return schemaSource; 
+		return schemaSource;
 	}
-	
+
 	/**
-	 * Returns the schema object that can be used for validating XML (e.g. DOM or SAX). 
+	 * Returns the target URL of a base URL after redirecting
+	 * @param urlString
+	 * @return Final URL
+	 * @throws IOException
+	 */
+	public static URL retrieveFinalURL(String urlString) throws IOException {
+		URL url = new URL(urlString);
+		URLConnection urlConnection = url.openConnection();
+
+		if(!(urlConnection instanceof JarURLConnection)){
+			HttpURLConnection con = (HttpURLConnection) urlConnection;
+			con.setInstanceFollowRedirects(false);
+			con.connect();
+			con.getInputStream();
+
+			if (con.getResponseCode() == HttpURLConnection.HTTP_MOVED_PERM || con.getResponseCode() == HttpURLConnection.HTTP_MOVED_TEMP) {
+				String redirectUrl = con.getHeaderField("Location");
+				return retrieveFinalURL(redirectUrl);
+			}
+		}
+
+		return url;
+	}
+
+	/**
+	 * Returns the schema object that can be used for validating XML (e.g. DOM or SAX).
 	 */
 	public Schema getSchema() {
 		if (schema == null) {
     		//SchemaFactory schemaFactory = SchemaFactory.newInstance("http://www.w3.org/2001/XMLSchema");
     		SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
 			try {
-				URL schemaSource = getSchemaSource();
+				URL schemaSource = retrieveFinalURL(getSchemaSource().toString());
 				InputStream inputStream = schemaSource.openStream();
 				Source src = new StreamSource(inputStream);
 				schema = schemaFactory.newSchema(src);
@@ -69,11 +98,11 @@ public class XmlValidator {
 				e.printStackTrace();
 			} catch (IOException e) {
 				e.printStackTrace();
-			} 
+			}
 		}
 		return schema;
 	}
-	
+
 	/**
 	 * Returns the schema version of this validator
 	 * @return Version object
